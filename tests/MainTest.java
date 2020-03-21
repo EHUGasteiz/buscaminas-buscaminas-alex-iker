858278cdc13bb3ce69998c2de18f6b8a8cfb2257
Alex <ass0100.csmm@gmail.com>
2020-03-21 19:26:21 +0100

    Clases Test
    Creadas las clases para realizar los test más adelante.

---
+++ tests/MainTest.java
@@ -0,0 +1,30 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import Controlador.Main;
+
+
+
+public class MainTest {
+	
+	
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		
+	}
+
+	@Test
+	public void testSolicitarDificultad() {
+		
+	}
+	
+	@Test
+	public void testCrearVentanaBuscaminas() {
+		
+	}
+	
+
+}
---
+++ tests/PartidaTest.java
@@ -0,0 +1,54 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import Modelo.Partida;
+
+public class PartidaTest {
+
+	
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		
+	}
+
+	@Test
+	public void testGetPartida() {
+		
+	}
+	@Test
+	public void testGenerarMinas() {
+		
+		
+	}
+	@Test
+	public void testRandInt() {
+		
+	}
+	@Test
+	public void testImprimirMinas() {
+		
+	}
+	@Test
+	public void testGetAncho() {
+		
+	}
+	@Test
+	public void testGetAlto() {
+		
+	}
+	@Test
+	public void testGetCantCasillas() {
+		
+	}
+	@Test
+	public void testCalcularCasillas() {
+		
+	}
+	
+}
